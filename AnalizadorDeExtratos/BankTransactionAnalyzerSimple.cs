74c37191eb30f6a46247cdc01169ca9ea3289265
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2023-09-03 15:15:13 -0300

    feat: primeiro examplo

---
+++ AnalizadorDeExtratos/BankTransactionAnalyzerSimple.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel;
+using System.IO;
+
+namespace AnalizadorDeExtratos;
+
+public class BankTransactionAnalyzerSimple
+{
+    private static readonly string RESOURCES = "../";
+
+    public static void Main(string[] args)
+    {
+        string[] readText = File.ReadAllLines(RESOURCES + args[0]);
+        GetTotalTransactions(readText);
+        GetTotalTransactionsForJanuary(readText);
+
+
+    }
+
+    public static void GetTotalTransactions(string[] file)
+    {
+        double total = 0d;
+        foreach (string lines in file)
+        {
+            string[] columns = lines.Split(',');
+            double amount = Convert.ToDouble(columns[1]);
+            DateTime date = Convert.ToDateTime(columns[0]);
+            total += amount;
+        }
+
+        Console.WriteLine("the total for all transactions is " + total);
+    }
+
+    public static void GetTotalTransactionsForJanuary(string[] file)
+    {
+        double total = 0d;
+        foreach (string lines in file)
+        {
+            string[] columns = lines.Split(',');
+            double amount = Convert.ToDouble(columns[1]);
+            DateTime date = Convert.ToDateTime(columns[0]);
+
+            if (date.Month == 1 )
+            total += amount;
+            {
+
+            }
+        }
+
+        Console.WriteLine("the total for all transactions in january is " + total);
+    }
+}
