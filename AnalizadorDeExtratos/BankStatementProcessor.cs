920e42cb25d7036ef25c0554adce6b1f58098c9c
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2023-09-03 17:35:16 -0300

    feat: extraindo logica de calculo para classe propria

---
+++ AnalizadorDeExtratos/BankStatementProcessor.cs
@@ -0,0 +1,51 @@
+﻿using System.Globalization;
+
+namespace AnalizadorDeExtratos
+{
+    public class BankStatementProcessor
+    {
+
+        public List<BankTransaction> BankTransactions { get; private set; }
+
+        public BankStatementProcessor(List<BankTransaction> bankTransactions)
+        {
+                BankTransactions = bankTransactions;
+        }
+
+        public  double CalculateTotalAmount()
+        {
+            double total = 0d;
+            foreach (BankTransaction bankTransaction in BankTransactions)
+            {
+                total += bankTransaction.Amount;
+            }
+            return total;
+        }
+
+        public double CalculateTotalInMonth(int monthIndex)
+        {
+            double total = 0d;
+            foreach (BankTransaction bankTransaction in BankTransactions)
+            {
+                if (bankTransaction.Date.Month == monthIndex)
+                {
+                    total += bankTransaction.Amount;
+                }
+            }
+            return total;
+        }
+
+        public double CalculateTotalForCategory(string category)
+        {
+            double total = 0d;
+            foreach (BankTransaction bankTransaction in BankTransactions)
+            {
+                if (bankTransaction.Description.Equals(category))
+                {
+                    total += bankTransaction.Amount;
+                }
+            }
+            return total;
+        }
+    }
+}
--- AnalizadorDeExtratos/BankTransactionAnalyzerSimple.cs
+++ AnalizadorDeExtratos/BankTransactionAnalyzerSimple.cs
@@ -1,8 +1,4 @@
-﻿using Microsoft.VisualBasic;
-using System.ComponentModel;
-using System.Globalization;
-using System.IO;
-using System.Runtime.Serialization;
+﻿using System.Globalization;
 
 namespace AnalizadorDeExtratos;
 
@@ -15,36 +11,18 @@ public class BankTransactionAnalyzerSimple
         var filename = args[0];
         var bankStatementParser = new BankStatementCSVParser();
         string[] readText = File.ReadAllLines(RESOURCES + filename);
+
         List<BankTransaction> bankTransactions = bankStatementParser.ParseLinesFromCSV(readText);
-        CalculateTotalAmount(bankTransactions);
-        SelectInMonth(bankTransactions, 2);
-        
+        var bankProcessor = new BankStatementProcessor(bankTransactions);
 
-
+        CollectSummary(bankProcessor);
     }
 
-    public static void CalculateTotalAmount(List<BankTransaction> bankTransactions)
+    public static void CollectSummary(BankStatementProcessor bankProcessor)
     {
-        double total = 0d;
-        foreach (BankTransaction bankTransaction in bankTransactions)
-        {
-            total += bankTransaction.Amount;
-        }
-
-        Console.WriteLine("the total for all transactions is " + total);
-    }
-
-    public static void SelectInMonth(List<BankTransaction> bankTransactions, int monthIndex)
-    {
-        double total = 0d;
-        foreach (BankTransaction bankTransaction in bankTransactions)
-        {
-            if (bankTransaction.Date.Month == monthIndex)
-            {
-                total += bankTransaction.Amount;
-            }
-        }
-
-        Console.WriteLine($"the total for all transactions in {CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(monthIndex)} is {total}");
+        Console.WriteLine("the total for all transactions is " + bankProcessor.CalculateTotalAmount());
+        Console.WriteLine("the total for all transactions in January is " + bankProcessor.CalculateTotalInMonth(1));
+        Console.WriteLine("the total for all transactions in Jebruary is " + bankProcessor.CalculateTotalInMonth(2));
+        Console.WriteLine("the total salary received is " + bankProcessor.CalculateTotalForCategory("Salary"));
     }
 }
