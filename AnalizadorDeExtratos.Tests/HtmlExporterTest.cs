69c337cd740c7ab9172e0b6c2900a29977033746
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2023-09-07 16:50:48 -0300

    Revert "Revert "test: exporter""
    This reverts commit 4142af473a49789a6492591fb7676b6233864ee0.

---
+++ AnalizadorDeExtratos.Tests/HtmlExporterTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalizadorDeExtratos.Tests
+{
+    public class HtmlExporterTest
+    {
+
+        [Fact]
+        public void ShouldReturnHtmlWithStatistics()
+        {
+            var htmlExporter = new HtmlExporter();
+            var statistics = new SummaryStatistics(12, 02, 21, 20);
+            var isSum = "<li><strong>The sum is</strong>: 20";
+            var isMax = "<li><strong>The max is</strong>: 12";
+            var isMin = "<li><strong>The min is</strong>: 2";
+            var isAverage = "<li><strong>The average is</strong>: 21";
+            var result = htmlExporter.Export(statistics);
+
+            Assert.Contains(isSum, result);
+            Assert.Contains(isMax, result);
+            Assert.Contains(isMin, result);
+            Assert.Contains(isAverage, result);
+        }
+    }
+}
